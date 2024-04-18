b6857d1234ffa9e69923a17e54c49c5d4cbf5e9a
Enzo Nether <133067606+Nether16@users.noreply.github.com>
2024-04-18 14:29:30 -0300

    Termino do Programa

--- Program.cs
+++ Program.cs
@@ -8,66 +8,99 @@ namespace Time_de_Herois
 {
     class Program
     {
-        static int pontuação1, pontuação2, pontuação3, pontuação4, pontuação5;
-        static string poder1, poder2, poder3, poder4, poder5;
-        static string heroi1 = "", heroi2 = "", heroi3 = "", heroi4 = "", heroi5 = "";
+        static bool loop = true, montado = false;
+        static int pontuação1, pontuação2, pontuação3, pontuação4, pontuação5, p1, p2, p3, quantidade;
+        static string poder1, poder2, poder3, poder4, poder5,escolha1,escolha2,escolha3,escolha = "", heroi1 = "", heroi2 = "", heroi3 = "", heroi4 = "", heroi5 = "";
         static void Main(string[] args)
         {
-            int quantidade = 1;
-            Console.WriteLine("Quantos Herois serão cadastrados (maximo 5)");
-            quantidade = int.Parse(Console.ReadLine());
+            while (loop)
+            {
+                Console.WriteLine("[1]Cadastrar Herois\r\n[2]Montar equipe \r\n[3]Ver equipe \r\n[4]Sair");
+                escolha = Console.ReadLine();
 
-            if (quantidade > 5 || quantidade < 1)
-            {
-                Console.WriteLine($"Nao é possivel cadastrar {quantidade} Herois");
-            }
-            else
-            {
-                for (int i = 1; i <= quantidade; i++)
+                switch (escolha)
                 {
-                    switch (i)
-                    {
-                        case 1:
-                            Console.WriteLine($"Insira o nome do {i}º Heroi");
-                            heroi1 = Console.ReadLine();
-                            poder1 = EscolhadosHerois(i);
-                            Console.WriteLine("Insira a pontuação de missoes dele");
-                            pontuação1 = int.Parse(Console.ReadLine());
-                            break;
-                        case 2:
-                            Console.WriteLine($"Insira o nome do {i}º Heroi");
-                            heroi2 = Console.ReadLine();
-                            poder2 = EscolhadosHerois(i);
-                            Console.WriteLine("Insira a pontuação de missoes dele");
-                            pontuação2 = int.Parse(Console.ReadLine());
-                            break;
-                        case 3:
-                            Console.WriteLine($"Insira o nome do {i}º Heroi");
-                            heroi3 = Console.ReadLine();
-                            poder3 = EscolhadosHerois(i);
-                            Console.WriteLine("Insira a pontuação de missoes dele");
-                            pontuação3 = int.Parse(Console.ReadLine());
-                            break;
-                        case 4:
-                            Console.WriteLine($"Insira o nome do {i}º Heroi");
-                            heroi4 = Console.ReadLine();
-                            poder4 = EscolhadosHerois(i);
-                            Console.WriteLine("Insira a pontuação de missoes dele");
-                            pontuação4 = int.Parse(Console.ReadLine());
-                            break;
-                        case 5:
-                            Console.WriteLine($"Insira o nome do {i}º Heroi");
-                            heroi5 = Console.ReadLine();
-                            poder5 = EscolhadosHerois(i);
-                            Console.WriteLine("Insira a pontuação de missoes dele");
-                            pontuação5 = int.Parse(Console.ReadLine());
-                            break;
-                    }
+                    case "1":
+                        Console.WriteLine("Quantos Herois serão cadastrados (maximo 5)");
+                        quantidade = int.Parse(Console.ReadLine());
 
+                        if (quantidade > 5 || quantidade < 1)
+                        {
+                            Console.WriteLine($"Nao é possivel cadastrar {quantidade} Herois");
+                        }
+                        else
+                        {
+                            for (int i = 1; i <= quantidade; i++)
+                            {
+                                switch (i)
+                                {
+                                    case 1:
+                                        Console.WriteLine($"Insira o nome do {i}º Heroi");
+                                        heroi1 = Console.ReadLine();
+                                        poder1 = EscolhadosHerois(i);
+                                        Console.WriteLine("Insira a pontuação de missoes dele");
+                                        pontuação1 = int.Parse(Console.ReadLine());
+                                        break;
+                                    case 2:
+                                        Console.WriteLine($"Insira o nome do {i}º Heroi");
+                                        heroi2 = Console.ReadLine();
+                                        poder2 = EscolhadosHerois(i);
+                                        Console.WriteLine("Insira a pontuação de missoes dele");
+                                        pontuação2 = int.Parse(Console.ReadLine());
+                                        break;
+                                    case 3:
+                                        Console.WriteLine($"Insira o nome do {i}º Heroi");
+                                        heroi3 = Console.ReadLine();
+                                        poder3 = EscolhadosHerois(i);
+                                        Console.WriteLine("Insira a pontuação de missoes dele");
+                                        pontuação3 = int.Parse(Console.ReadLine());
+                                        break;
+                                    case 4:
+                                        Console.WriteLine($"Insira o nome do {i}º Heroi");
+                                        heroi4 = Console.ReadLine();
+                                        poder4 = EscolhadosHerois(i);
+                                        Console.WriteLine("Insira a pontuação de missoes dele");
+                                        pontuação4 = int.Parse(Console.ReadLine());
+                                        break;
+                                    case 5:
+                                        Console.WriteLine($"Insira o nome do {i}º Heroi");
+                                        heroi5 = Console.ReadLine();
+                                        poder5 = EscolhadosHerois(i);
+                                        Console.WriteLine("Insira a pontuação de missoes dele");
+                                        pontuação5 = int.Parse(Console.ReadLine());
+                                        break;
+                                }
+
+                            }
+                        }
+                        Console.ReadLine();
+                        break;
+                    case "2":
+                        if (quantidade != 0)
+                        {
+                            Elenco(quantidade, heroi1, heroi2, heroi3, heroi4, heroi5, true);
+                            montado = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Cadastre herois primeiro antes de montar o time");
+                        }
+                        break;
+                    case "3":
+                        if (montado == true)
+                        {
+                            MontagemdeTime(escolha1, escolha2, escolha3, p1, p2, p3);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Monte um time antes");
+                        }
+                        break;
+                    case "4":
+                        loop = false;
+                        break;
                 }
-                Elenco(quantidade, heroi1, heroi2, heroi3, heroi4, heroi5,true);
             }
-            Console.ReadLine();
         }
         static string EscolhadosHerois(int quant)
         {
@@ -94,7 +127,7 @@ namespace Time_de_Herois
                         Console.WriteLine(h4 + $" e tem uma pontução em missoes de {pontuação4}");
                         break;
                     case 5:
-                        Console.WriteLine($"O heroi {heroi1}"+ h5 + $" e tem uma pontução em missoes de {pontuação5}");
+                        Console.WriteLine($"O heroi {heroi1}" + h5 + $" e tem uma pontução em missoes de {pontuação5}");
                         break;
                 }
             }
@@ -102,27 +135,25 @@ namespace Time_de_Herois
             {
                 Console.WriteLine("Escolha 3 dos herois para fazerem parte do seu time e coloque a pontuação deles");
                 Console.Write("Primeiro Heroi : ");
-                string escolha1 = Console.ReadLine();
-
+                escolha1 = Console.ReadLine();
                 Console.Write("Segundo Heroi : ");
-                string escolha2 = Console.ReadLine();
-
+                escolha2 = Console.ReadLine();
                 Console.Write("Terceiro Heroi : ");
-                string escolha3 = Console.ReadLine();
+                escolha3 = Console.ReadLine();
 
-                int p1 = Checagem(escolha1, quant);
-                int p2 = Checagem(escolha2, quant);
-                int p3 = Checagem(escolha3, quant);
-                MontagemdeTime(escolha1,escolha2,escolha3,p1,p2,p3);
+                p1 = Checagem(escolha1, quant);
+                p2 = Checagem(escolha2, quant);
+                p3 = Checagem(escolha3, quant);
+                MontagemdeTime(escolha1, escolha2, escolha3, p1, p2, p3);
             }
         }
-        static void MontagemdeTime(string h1, string h2, string h3, int p1, int p2, int p3)
+        static void MontagemdeTime(string h1, string h2, string h3, int po1, int po2, int po3)
         {
-            Console.WriteLine($"Voce montou um time com \r\n{h1} com uma pontuação de {p1}\r\n{h2} com uma pontuação de {p2}\r\n{h3} com uma pontuação de {p3}");
-            int total = p1 + p2 + p3;
+            Console.WriteLine($"Voce montou um time com \r\n{h1} com uma pontuação de {po1}\r\n{h2} com uma pontuação de {po2}\r\n{h3} com uma pontuação de {po3}");
+            int total = po1 + po2 + po3;
             Console.WriteLine($"A pontuação total de seu time é : {total}");
         }
-        static int Checagem(string nome,int quant)
+        static int Checagem(string nome, int quant)
         {
             int pont = 0;
             for (int i = 1; i <= quant; i++)
